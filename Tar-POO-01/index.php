f05c5ae0dfd21208f6ca09d547449586ff3a3d1c
Domingo013 <dminoredondo.guadalupe@alumnado.fundacionloyola.net>
2023-05-06 21:22:42 +0200

    Saber por qué palabra empieza la consulta Completado en la tarea POO-01

--- Tar-POO-01/index.php
+++ Tar-POO-01/index.php
@@ -5,35 +5,37 @@
 
     if(isset($_POST["ejecutar"])){      // Si se pulsa el botón de ejecutar
         $consultas = new Consultas();
-        $sql1 = $consultas->consultaSelect($_POST["consulta"]);   // Envío la consulta a la función de la clase
-        $numFilas = $consultas->numFilas;
+        $primera = strtolower(substr($_POST["consulta"], 0, 6));     // Pasamos a minúsculas toda la consulta SQL y nos quedamos con las 6 primeras letras
+        if($primera == 'select'){
+            $sql1 = $consultas->consultaSelect($_POST["consulta"]);   // Envío la consulta a la función de la clase
+            $numFilas = $consultas->numFilas;
 
-        $aux = 1;       // Variable para bandera
-        if($numFilas > 0){      // Si ha devuelto 1 o más filas, crea la tabla con los valores
-            echo '<table>';
-            while($fila=$sql1->fetch_assoc()){      // Recorre el array para sacar los datos
-                if($aux == 1){      // Solo entra 1 vez ya que $aux pasa a valer 0
+            $aux = 1;       // Variable para bandera
+            if($numFilas > 0){      // Si ha devuelto 1 o más filas, crea la tabla con los valores
+                echo '<table>';
+                while($fila=$sql1->fetch_assoc()){      // Recorre el array para sacar los datos
+                    if($aux == 1){      // Solo entra 1 vez ya que $aux pasa a valer 0
+                        echo '<tr>';
+                        foreach($fila as $indice=>$valor){      // Para mostrar el nombre de las columnas
+                            echo '<th>'.$indice.'</th>';
+                        }
+                        echo '</tr>';
+                    }
+                    $aux = 0;
                     echo '<tr>';
-                    foreach($fila as $indice=>$valor){      // Para mostrar el nombre de las columnas
-                        echo '<th>'.$indice.'</th>';
+                    foreach($fila as $indice=>$valor){      // Para mostrar las filas de cada columna de la tabla
+                        echo '<td>'.$valor.'</td>';
                     }
                     echo '</tr>';
                 }
-                $aux = 0;
-                echo '<tr>';
-                foreach($fila as $indice=>$valor){      // Para mostrar las filas de cada columna de la tabla
-                    echo '<td>'.$valor.'</td>';
-                }
-                echo '</tr>';
+                echo '</table>';
+            }else{
+                echo "No se ha devuelto nada";
             }
-            echo '</table>';
-        }else{
-            echo "No se ha devuelto nada";
+        }elseif($primera == 'insert'){      // Si se pulsa el botón de ejecutar
+            $consultas = new Consultas();   
+            $sql1 = $consultas->consultaActualizar($_POST["consulta"]);   // Envío la consulta a la función de la clase
         }
-    }
-    if(isset($_POST["actualizar"])){      // Si se pulsa el botón de ejecutar
-        $consultas = new Consultas();   
-        $sql1 = $consultas->consultaActualizar($_POST["consulta"]);   // Envío la consulta a la función de la clase
     }
     include_once("vistas/finhtml.php");     // Muestra el contenido html de finhtml.php
 ?>
--- Tar-POO-01/vistas/header.php
+++ Tar-POO-01/vistas/header.php
@@ -12,8 +12,7 @@
             Introduce la consulta a ejecutar:
             <form action="index.php" method="POST">
                 <input type="text" name="consulta" id="consulta"><br>
-                <input type="submit" name="ejecutar" value="SELECT" id="boton">   <!-- En el caso de ser una consulta Select -->
-                <input type="submit" name="actualizar" value="ACTUALIZAR" id="boton">   <!-- En el caso de ser una consulta de actualizar -->
+                <input type="submit" name="ejecutar" value="EJECUTAR" id="boton">   <!-- En el caso de ser una consulta Select -->
             </form>
         </div><br><br>
         <div id="resultado">
